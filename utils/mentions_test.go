81275e1aa88977213ead596b2c80a808f67534ce
Terisback <terisbackno@gmail.com>
2020-07-07 12:06:24 +0500

    Delete not useless tests

--- utils/mentions_test.go
+++ utils/mentions_test.go
@@ -32,18 +32,6 @@ func TestGetIDFromArg(t *testing.T) {
 			wantId: id,
 			wantOk: true,
 		},
-		{
-			name:   "DefaultMention",
-			args:   args{arg: fmt.Sprintf("<@%d>", id)},
-			wantId: id,
-			wantOk: true,
-		},
-		{
-			name:   "MentionWithArgs",
-			args:   args{arg: fmt.Sprintf("<@%d> online %d", id, otherID)},
-			wantId: id,
-			wantOk: true,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
