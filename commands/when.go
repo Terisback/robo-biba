8bfcf2b8c7e39e33a0c362c5b0bfbaad0de9290e
Anton Terisback <terisbackno@gmail.com>
2020-07-14 04:35:16 +0500

    When command refactor - removing useless features

--- commands/when.go
+++ commands/when.go
@@ -11,20 +11,28 @@ import (
 
 func When(session disgord.Session, event *disgord.MessageCreate) {
 	var member *disgord.Member
-	var thereIsMention bool
 	command, err := middleware.GetCommandFromContext(event.Ctx)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	args := command.Arguments
+	var (
+		embed    = disgord.Embed{}
+		nickname string
+		args     = command.Arguments
+	)
 
 	if len(args) == 1 {
-		serverCreatedDate := event.Message.GuildID.Date()
-		_, err := event.Message.Reply(context.Background(), session,
-			serverCreatedDate.Format("Сервер был создан 02.01.2006 в 15:04:05 по Москве"),
-		)
+		nickname = event.Message.Member.Nick
+
+		if nickname == "" {
+			nickname = event.Message.Author.Username
+		}
+
+		userJoinedAtDate := event.Message.Member.JoinedAt
+		embed.Description = "**" + nickname + userJoinedAtDate.Format("** зашел на сервер 02.01.2006 в 15:04:05 по Москве")
+		_, err := event.Message.Reply(context.Background(), session, &embed)
 		if err != nil {
 			log.Println(err)
 		}
@@ -32,38 +40,26 @@ func When(session disgord.Session, event *disgord.MessageCreate) {
 	}
 
 	if len(args) < 2 {
-		// TODO: Return help
+		embed.Description = "`!когда <id или упоминание>` - Узнать когда пользователь зашёл на сервер"
+		_, err := event.Message.Reply(context.Background(), session, &embed)
+		if err != nil {
+			log.Println(err)
+		}
 		return
-	}
-
-	var nickname string
-	thereIsMention = false
-
-	member, err = session.GetMember(context.Background(), event.Message.GuildID, event.Message.Author.ID)
-	if err != nil {
-		return
-	}
-	nickname = member.Nick
-
-	if nickname == "" {
-		nickname = member.User.Username
 	}
 
 	if _, ok := args[1].GetID(); ok {
-		member, thereIsMention = utils.GetMemberFromArg(session, event.Message.GuildID, args[1].String())
+		member, _ = utils.GetMemberFromArg(session, event.Message.GuildID, args[1].String())
 
 		nickname = member.Nick
 
 		if nickname == "" {
 			nickname = member.User.Username
 		}
-	}
 
-	if thereIsMention {
-		userCreatedDate := member.JoinedAt.Time
-		_, err = event.Message.Reply(context.Background(), session,
-			"**"+nickname+userCreatedDate.Format("** зашёл на сервер 02.01.2006 в 15:04:05 по Москве"),
-		)
+		userJoinedAtDate := member.JoinedAt.Time
+		embed.Description = "**" + nickname + userJoinedAtDate.Format("** зашёл на сервер 02.01.2006 в 15:04:05 по Москве")
+		_, err = event.Message.Reply(context.Background(), session, &embed)
 		if err != nil {
 			log.Println(err)
 			return
@@ -72,54 +68,24 @@ func When(session disgord.Session, event *disgord.MessageCreate) {
 
 	// Return created date from Author ID
 	if utils.Aliases(args[1].String(), "я", "мой", "me", "my") {
-		userCreatedDate := event.Message.Author.ID.Date()
-		_, err := event.Message.Reply(context.Background(), session,
-			"**"+nickname+userCreatedDate.Format("** был создан 02.01.2006 в 15:04:05 по Москве"),
-		)
-		if err != nil {
-			log.Println(err)
-		}
-		return
-	}
-
-	if len(args) < 3 {
-		// TODO: Return help
-		return
-	}
-
-	thereIsMention = false
-
-	if _, ok := args[2].GetID(); ok {
-		member, thereIsMention = utils.GetMemberFromArg(session, event.Message.GuildID, args[2].String())
-
-		nickname = member.Nick
+		nickname = event.Message.Member.Nick
 
 		if nickname == "" {
-			nickname = member.User.Username
+			nickname = event.Message.Author.Username
 		}
-	}
 
-	if thereIsMention && utils.Aliases(args[1].String(), "создан", "зарегался", "registered", "reg", "created") {
-		userCreatedDate := member.User.ID.Date()
-		_, err := event.Message.Reply(context.Background(), session,
-			"**"+nickname+userCreatedDate.Format("** был создан 02.01.2006 в 15:04:05 по Москве"),
-		)
+		userJoinedAtDate := event.Message.Member.JoinedAt
+		embed.Description = "**" + nickname + userJoinedAtDate.Format("** зашел на сервер 02.01.2006 в 15:04:05 по Москве")
+		_, err := event.Message.Reply(context.Background(), session, &embed)
 		if err != nil {
 			log.Println(err)
-			return
 		}
+		return
 	}
 
-	if thereIsMention && utils.Aliases(args[1].String(), "зашёл", "зашел", "присоединился", "joined", "join") {
-		userCreatedDate := member.JoinedAt.Time
-		_, err = event.Message.Reply(context.Background(), session,
-			"**"+nickname+userCreatedDate.Format("** зашёл на сервер 02.01.2006 в 15:04:05 по Москве"),
-		)
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	embed.Description = "`!когда <id или упоминание>` - Узнать когда пользователь зашёл на сервер"
+	_, err = event.Message.Reply(context.Background(), session, &embed)
+	if err != nil {
+		log.Println(err)
 	}
-
-	// Return help
 }
